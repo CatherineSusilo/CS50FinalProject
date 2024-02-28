8d4e954938c55a26ce3ae73c5acdddef928a2d48
Catherine <125679773+CatherineSusilo@users.noreply.github.com>
2024-02-28 20:53:43 +0700

    Created app.py
    i worked till login n register but i dont hv the database yet. gta make database

---
+++ app.py
@@ -0,0 +1,154 @@
+import os
+
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+
+import hashlib
+
+app = Flask(__name__)
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure SQLite database connect and cursor
+con = sqlite3.connect("ticketing.db")
+cur = con.cursor()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+@app.route('/')
+@login_required
+def dashboard():
+
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("userid"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        # DOESN'T WORK YET (gta make database)
+        rows = cur.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        con.commit()
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+        elif not request.form.get("password", 400):
+            return apology("must provide password", 400)
+        elif not request.form.get("confirmation"):
+            return apology("must confirm password", 400)
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords do not match", 400)
+        rows = cur.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        con.commit()
+        if len(rows) != 0:
+            return apology("username already exists", 400)
+
+        # DOESN'T WORK YET (gta make database)
+        con.execute("INSERT INTO users (username, hash) VALUES(?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
+        cur.commit()
+
+        # DOESN'T WORK YET (gta make database)
+        rows = cur.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        con.commit()
+
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+    else:
+        return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
